21e6c7fb4bed2626ee8b6bda4a0700ee8d251d4d
Carlos Costa <carlossergiosilvacosta@gmail.com>
2024-06-07 22:33:03 -0300

    first commit

---
+++ src/main/java/com/unicap/sistema_gerenciamento_projetos/SistemaGerenciamentoProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.unicap.sistema_gerenciamento_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicap/sistema_gerenciamento_projetos/SistemaGerenciamentoProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicap.sistema_gerenciamento_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
